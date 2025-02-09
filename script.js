cd06cfc2371a586b195063accdf80cc6dc90e952
Muniz2811 <pedrohenben2014@gmail.com>
2025-02-08 23:54:38 -0300

    Seleçao de itens

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-let items = [
-    { id: 1, name: 'Pistola', weight: 2, value: 8, image: 'assets/images/handgun.png' },
-    { id: 2, name: 'Espingarda', weight: 4, value: 10, image: 'assets/images/RE4R_W-870.png' },
-    { id: 3, name: 'Ervas Verdes', weight: 1, value: 5, image: 'assets/images/Green.png' },
-];
-
-let selectedItems = new Set();
-function createItemElement(item) {
-    const div = document.createElement('div'); div.className = 'item';
+function createItemElement(item, isSelected) {
+    const div = document.createElement('div');
+    div.className = `item ${isSelected ? 'selected' : ''}`;
     div.innerHTML = `
-    <img src="${item.image}" alt="${item.name}">
-    <div class="item-name">${item.name}</div>
-    <div class="item-stats">
-    <span>Peso: ${item.weight}</span>
-    <span>Valor: ${item.value}</span>
-    </div>
+        <img src="${item.image}" alt="${item.name}">
+        <div class="item-name">${item.name}</div>
+        <div class="item-stats">
+            <span>Peso: ${item.weight}</span>
+            <span>Valor: ${item.value}</span>
+        </div>
     `;
+    div.addEventListener('click', () => {
+        if (selectedItems.has(item.id)) {
+            selectedItems.delete(item.id);
+        } else {
+            selectedItems.add(item.id);
+        }
+        updateDisplay();
+    });
     return div;
 }
 
 function updateDisplay() {
     const itemsList = document.getElementById('itemsList');
     const selectedItemsDiv = document.getElementById('selectedItems');
+    
     itemsList.innerHTML = '';
     selectedItemsDiv.innerHTML = '';
+    
     items.forEach(item => {
-        const itemElement = createItemElement(item);
-        itemsList.appendChild(itemElement);
+        const isSelected = selectedItems.has(item.id);
+        const itemElement = createItemElement(item, isSelected);
+        
+        if (isSelected) {
+            selectedItemsDiv.appendChild(itemElement);
+        } else {
+            itemsList.appendChild(itemElement);
+        }
     });
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    updateDisplay();
-});
